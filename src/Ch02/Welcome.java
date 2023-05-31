09c1f2b07cfaa096b96006b140fa6f947ad04251
oraman <oraman@naver.com>
2023-05-31 10:50:54 +0900

    pull된 파일을 다시 push해보는 테스트입니다

--- src/Ch02/Welcome.java
+++ src/Ch02/Welcome.java
@@ -2,6 +2,8 @@ package Ch02;
 
 public class Welcome {
     public static void main(String[] args) {
+
         System.out.println("Welcome01!");
+        System.out.println("Welcome02?");
     }
 }
