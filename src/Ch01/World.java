23705f42bf320d9613f57cad427f6fd78b9e3a63
oraman <oraman@naver.com>
2023-05-31 10:30:44 +0900

    훠어어어억힝 월드

--- src/Ch01/World.java
+++ src/Ch01/World.java
@@ -3,6 +3,6 @@ package Ch01;
 public class World {
     public static void main(String[] args) {
         System.out.println("World01!");
-        System.out.println("World02?");
+        System.out.println("World02");
     }
 }
